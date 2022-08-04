16bd7fa78a407c0d96566f86754b09c3179b366c
Mar Alexis Omay <47245545+MarOmay@users.noreply.github.com>
2022-08-04 19:25:48 +0800

    Fixed: LogIn onBackPressed

--- app/src/main/java/com/silong/admin/LogIn.java
+++ app/src/main/java/com/silong/admin/LogIn.java
@@ -357,6 +357,14 @@ public class LogIn extends AppCompatActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     protected void onDestroy() {
         // Unregister since the activity is about to be closed.
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
