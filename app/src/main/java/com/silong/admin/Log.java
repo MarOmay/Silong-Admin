6cb3827f6278e48ad8be4d7b9b5fefd6512d4adf
Mar Alexis Omay <47245545+MarOmay@users.noreply.github.com>
2022-10-08 09:18:37 +0800

    Fixed: Log (reset static variables)

--- app/src/main/java/com/silong/admin/Log.java
+++ app/src/main/java/com/silong/admin/Log.java
@@ -62,9 +62,6 @@ public class Log extends AppCompatActivity {
         window.setStatusBarColor(this.getResources().getColor(R.color.pink));
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
-        LOGDATA.clear();
-        customDate = false;
-
         //register receiver
         LocalBroadcastManager.getInstance(this).registerReceiver(mReloadReceiver, new IntentFilter("refresh-logs"));
 
@@ -79,6 +76,14 @@ public class Log extends AppCompatActivity {
 
         LogsFetcher logsFetcher = new LogsFetcher(Log.this);
         logsFetcher.execute();
+
+        //reset static variables
+        LOGDATA.clear();
+        EXPORTABLE.clear();
+
+        dateFrom = Utility.dateToday().replace("-","/");
+        dateTo = Utility.dateToday().replace("-","/");
+        customDate = false;
 
         loadData();
 
